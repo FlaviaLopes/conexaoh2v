59f997913e674d45cea600acd4ee19e10022a3a2
Robson Caldas <len@rcaldas.com>
2022-12-18 00:10:21 -0300

    Script Init para migrations, restaurar backup do banco e createsuperuser

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "\n - Apply database migrations"
+docker compose exec django python manage.py makemigrations
+docker compose exec django python manage.py migrate
+
+echo -e "\n - Restore database"
+docker compose cp conexaoh2v.sql mysql:/dump.sql
+docker compose exec mysql /bin/bash -c 'MYSQL_PWD=$MYSQL_ROOT_PASSWORD mysql --one-database conexaoh2v < /dump.sql'
+docker compose exec mysql rm /dump.sql
+
+echo -e "\n - Creating superuser"
+docker compose exec -it django python manage.py createsuperuser
